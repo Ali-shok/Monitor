e110a2649012da23dc754fcef992102edfe0e4bf
Your Name <you@example.com>
2022-08-13 00:42:22 +0300

    Create Monitor

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header></header>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
